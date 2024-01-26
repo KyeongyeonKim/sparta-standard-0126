f5547c75670b40d00b24a4334f0e630efe1a2474
KY <rudyeon@naver.com>
2024-01-26 22:21:11 +0900

    hw-ex4

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -29,8 +29,8 @@ function FilterButtons(
   return (
     <div>
       {/* 여기에 필터링 버튼들을 완성하세요. */}
-      <button onClick={filterByAge}>18세 이상</button>
-      <button onClick={filterByGrade}>A등급</button>
+      <button onClick={() => filterByAge(18)}>18세 이상</button>
+      <button onClick={() => filterByGrade("A")}>A등급</button>
       <button onClick={resetFilter}>필터 초기화</button>
     </div>
   );
